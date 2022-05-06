77cf60f6e948983dec212729ec97e5a03e86cd55
Acid <77523049+Acid-v@users.noreply.github.com>
2022-05-06 12:07:45 +0800

    Create test.c

---
+++ day 15/test.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int i=0;
+//	for(i=0;i<100;i++)
+//	{
+//	printf("%d ",i);
+//	
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+
+//F5启动调试-和F9配合使用的
+//int main()
+//{
+//	int i=0;
+//	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	for(i=0;i<=12;i++)
+//	{
+//		printf("hehe\n");
+//		arr[i]=0;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//Fn+F9切换断点
+//Fn+F10 逐过程调试
+//int main()
+//{
+//	int i=0;
+//	for(i=0;i<=100;i++)
+//	{
+//		printf("%d",i);
+//	}
+//	for(i=0;i<=100;i++)
+//	{
+//	    printf("%d",10-i);
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+//int Add(int x,int y)
+//{
+//	return x+y;
+//}
+//int main()
+//{
+//	int a=20;
+//	int b=10;
+//	int c=Add(a,b);
+//	printf("hehe\n");
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	{
+//	int tmp=0;
+//	printf("tmp=%d\n",tmp);
+//	}
+//	int arr[10]={0};
+//	int i=0;
+//	for(i=0;i<10;i++)
+//	{
+//	arr[i]=i;
+//	
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i=0;
+//	int sum=0;
+//	int n=0;
+//	int ret=1;
+//	scanf("%d",&n);
+//	for(i=1;i<=n;i++)
+//	{
+//		int j=0;
+//		ret =1;
+//		for(j=1;j<=i;j++)
+//		{
+//			ret*=j;
+//		}
+//		sum+=ret;
+//	}
+//	printf("%d\n",sum);
+//	return 0;
+//}
